8cb8ff576618ed72e37d71e9107a33bd30b0761e
ddijk <dick@dijkrosoft.nl>
2021-12-18 18:03:02 +0100

    refactored to use parametrized tests

--- test__merge_sort.py
+++ test__merge_sort.py
@@ -1,43 +1,41 @@
 from merge_sort import merge_sort, merge
 import pytest
 
-@pytest.mark.parametrize('expected,input', [([1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8])])
+
+@pytest.mark.parametrize(
+    "expected,input", [([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8])]
+)
 def test_already_sorted(expected, input):
-    assert expected==merge_sort(input)
+    assert expected == merge_sort(input)
 
-@pytest.mark.parametrize('expected,input', [([1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8]), ([1,2,3,4,5,6,7,8],[1,2,3,5,4,6,7,8])])
-def test_sort(expected, input):
-    assert expected==merge_sort(input)
 
-def test_sort_2():
-    assert [1,2,3,4,5,6,7,8]==merge_sort([8,2,3,5,7,6,4,1])
+@pytest.mark.parametrize(
+    "expected,input",
+    [
+        ([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8]),
+        ([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 5, 4, 7, 6, 8]),
+        ([1, 2, 3, 4, 5, 6, 7, 8], [8, 2, 3, 5, 7, 6, 4, 1]),
+        ([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 5, 4, 6, 7, 8]),
+    ],
+)
+def test_merge_sort(expected, input):
+    assert expected == merge_sort(input)
 
-def test_merge_1():
-    assert [1,2,3] == merge([], [1,2,3])
 
-def test_merge_2():
-    assert [1,2,3] == merge([1,2,3], [])
-
-def test_merge_3():
-    assert [1,2,3] == merge([1,3], [2])
-
-def test_merge_4():
-    assert [1,2,3] == merge([2], [1,3])
-
-def test_merge_5():
-    assert [1,2,3,4] == merge([2,4], [1,3])
-
-def test_merge_6():
-    assert [1,2,3,4] == merge([1,3], [2,4])
-
-def test_merge_7():
-    assert [1,2,3,4,5,6,7,8] == merge([1,3,5,6], [2,4,7,8])
-
-def test_merge_8():
-    assert [1,2,3,4,5,6,7,8] == merge([2,4,7,8], [1,3,5,6])
-
-def test_merge_9():
-    assert [1,2,2, 3, 3,4,5,6,7,8] == merge([2,4,7,8], [1,2,3,3,5,6])
-
-def test_merge_10():
-    assert [1,2,2, 3, 3,4,5,6,7,8] == merge([1,2,3,3,5,6],[2,4,7,8])
+@pytest.mark.parametrize(
+    "expected,left,right",
+    [
+        ([1, 2, 3], [], [1, 2, 3]),
+        ([1, 2, 3], [1, 2, 3], []),
+        ([1, 2, 3], [1, 3], [2]),
+        ([1, 2, 3], [2], [1, 3]),
+        ([1, 2, 3, 4], [2, 4], [1, 3]),
+        ([1, 2, 3, 4], [1, 3], [2, 4]),
+        ([1, 2, 3, 4, 5, 6, 7, 8], [1, 3, 5, 6], [2, 4, 7, 8]),
+        ([1, 2, 3, 4, 5, 6, 7, 8], [2, 4, 7, 8], [1, 3, 5, 6]),
+        ([1, 2, 2, 3, 3, 4, 5, 6, 7, 8], [2, 4, 7, 8], [1, 2, 3, 3, 5, 6]),
+        ([1, 2, 2, 3, 3, 4, 5, 6, 7, 8], [1, 2, 3, 3, 5, 6], [2, 4, 7, 8]),
+    ],
+)
+def test_merge(expected, left, right):
+    assert expected == merge(left, right)
