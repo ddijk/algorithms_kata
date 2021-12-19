397434f2378f51ba6c882ad4e485cdfc30930a64
ddijk <dick@dijkrosoft.nl>
2021-12-19 16:55:25 +0100

    refactor to make more elegant

--- intersection.py
+++ intersection.py
@@ -1,7 +1,5 @@
 def intersect(a,b):
     result = []
     a_dict = {e: True for e in a}
-    for e in b:
-        if a_dict.get(e):
-            result.append(e)
+    [result.append(e) for e in b if a_dict.get(e)]
     return result
