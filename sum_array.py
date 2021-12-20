dbec00d2e262ddf8f5fd7ab26021b42488dd51cd
ddijk <dick@dijkrosoft.nl>
2021-12-20 21:24:14 +0100

    sum array

---
+++ sum_array.py
@@ -0,0 +1,16 @@
+import sys
+
+def sum(array):
+# Base case: only one element in the array: 
+    if len(array) == 1:
+        return array[0]
+    return array[0] + sum(array[1: len(array)- 1]) 
+
+def main():
+
+    input =sys.argv[1:]
+    print('input is ',input)
+    print(sum(map(lambda e: int(e), input)))
+
+if __name__=='__main__':
+    main()
