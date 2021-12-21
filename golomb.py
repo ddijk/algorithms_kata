a14c20a1af6a1bb0c3ac84674428b9605ef9e218
ddijk <dick@dijkrosoft.nl>
2021-12-21 19:28:31 +0100

    add memoization

--- golomb.py
+++ golomb.py
@@ -1,10 +1,14 @@
 import sys
 
-def golomb(n):
+def golomb(n, memo={}):
     if n == 1:
         return 1
 
-    return 1 + golomb(n - golomb(golomb(n - 1)))
+    if not memo.get(n):
+         res = 1 + golomb(n - golomb(golomb(n - 1)))
+         memo[n]=res
+
+    return memo[n]
 
 def main():
     print(golomb(int(sys.argv[1])))
